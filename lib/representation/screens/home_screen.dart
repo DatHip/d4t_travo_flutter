e373ee84c0c33225be439fc7f5ea40a32bd7f7a3
Tien Dat <83833987+DatHip@users.noreply.github.com>
2024-03-03 12:25:03 +0700

    -Update

--- lib/representation/screens/home_screen.dart
+++ lib/representation/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'package:d4t_travo_flutter/core/constants/dimension_constants.dart';
+import 'package:d4t_travo_flutter/representation/widgets/app_bar_container.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -12,6 +14,11 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const AppBarContainer(
+      titleString: "home",
+      implementLeading: false,
+      title: Padding(padding: EdgeInsets.symmetric(horizontal: kItemPadding)),
+      child: Column(),
+    );
   }
 }
