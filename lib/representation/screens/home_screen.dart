8e6240006e849244552884850febfee8095e2fb6
Tien Dat <83833987+DatHip@users.noreply.github.com>
2024-06-19 23:10:09 +0700

    -Update

--- lib/representation/screens/home_screen.dart
+++ lib/representation/screens/home_screen.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:d4t_travo_flutter/core/constants/dimension_constants.dart';
 import 'package:d4t_travo_flutter/representation/widgets/app_bar_container.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -12,13 +15,62 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  get child => null;
+
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  void _login() async {
+    var url = Uri.https('zozo-audio.aedev.net', "api/login");
+    var response = await http.post(url, body: {
+      'username': _usernameController.text,
+      'password': _usernameController.text
+    });
+    print(response.body);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const AppBarContainer(
+    return AppBarContainer(
       titleString: "home",
       implementLeading: false,
-      title: Padding(padding: EdgeInsets.symmetric(horizontal: kItemPadding)),
-      child: Column(),
+      title:
+          Padding(padding: EdgeInsets.symmetric(horizontal: kItemPadding + 10)),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+            child: TextField(
+              controller: _usernameController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'username',
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+            child: TextField(
+              controller: _passwordController,
+              obscureText: true,
+              enableSuggestions: false,
+              autocorrect: false,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'password',
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+            child: ElevatedButton(
+              onPressed: _login,
+              child: Text('Login'),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
